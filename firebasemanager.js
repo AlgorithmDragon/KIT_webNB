56b7392ad1d1891957c3f47c04382039c53059a1
AlgorithmDragon <146241981+AlgorithmDragon@users.noreply.github.com>
2023-10-10 19:26:50 +0900

    Update firebasemanager.js

--- firebasemanager.js
+++ firebasemanager.js
@@ -1,21 +1,19 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
 const firebaseConfig = {
-    piKey: "AIzaSyD8j6fUUKRklaF7AtxfWnNlcbSe-QRaLkE",
-    authDomain: "kitwebnb.firebaseapp.com",
-    databaseURL: "https://kitwebnb-default-rtdb.firebaseio.com",
-    projectId: "kitwebnb",
-    storageBucket: "kitwebnb.appspot.com",
-    messagingSenderId: "770056861850",
-    appId: "1:770056861850:web:6c155ce60dbe3d877ac723",
-    measurementId: "G-NC63BL75T8"
-  };
+  apiKey: "AIzaSyD8j6fUUKRklaF7AtxfWnNlcbSe-QRaLkE",
+  authDomain: "kitwebnb.firebaseapp.com",
+  databaseURL: "https://kitwebnb-default-rtdb.firebaseio.com",
+  projectId: "kitwebnb",
+  storageBucket: "kitwebnb.appspot.com",
+  messagingSenderId: "770056861850",
+  appId: "1:770056861850:web:6c155ce60dbe3d877ac723",
+  measurementId: "G-NC63BL75T8"
+};
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-const database = firebase.database();
-
-
-export {firebase, database};
-export {firebase, auth }; 
+const database = getDatabase(app);
+export {database, firebaseConfig };
